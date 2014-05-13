aebbed26c476e9dc860c25b6c430591d282fc1e3
fan0114 <fan0114@gmail.com>
2014-05-13 16:55:50 -0400

    asdf

---
+++ src/main/java/cs5412/project/distributed_file_system/pojo/History.java
@@ -0,0 +1,91 @@
+package cs5412.project.distributed_file_system.pojo;
+
+import java.sql.Timestamp;
+
+public class History {
+	private int hid;
+	private Timestamp ts;
+	private int uid;
+	private int oldFid;
+	private int newFid;
+	private int operationType;
+	private String oldName = "";
+
+	public int getHid() {
+		return hid;
+	}
+
+	public void setHid(int hid) {
+		this.hid = hid;
+	}
+
+	public Timestamp getTs() {
+		return ts;
+	}
+
+	public void setTs(Timestamp ts) {
+		this.ts = ts;
+	}
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public int getOldFid() {
+		return oldFid;
+	}
+
+	public void setOldFid(int oldFid) {
+		this.oldFid = oldFid;
+	}
+
+	public int getNewFid() {
+		return newFid;
+	}
+
+	public void setNewFid(int newFid) {
+		this.newFid = newFid;
+	}
+
+	public int getOperationType() {
+		return operationType;
+	}
+
+	public String getOT() {
+		if (operationType == 1) {
+			return "create";
+		}
+		if (operationType == 2) {
+			return "delete";
+		}
+		if (operationType == 3) {
+			return "update";
+		}
+		return "";
+	}
+
+	public void setOperationType(int operationType) {
+		this.operationType = operationType;
+	}
+
+	public String getOldName() {
+		return oldName;
+	}
+
+	public void setOldName(String oldName) {
+		this.oldName = oldName;
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other == null)
+			return false;
+		History otherFile = (History) other;
+		return this.hid == otherFile.getHid();
+	}
+
+}
